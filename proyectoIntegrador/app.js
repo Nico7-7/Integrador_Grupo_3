781c851d2766c76d5319d7ba674eb34daa44dff3
Nicolas Cavalieri <ncavalieri@udesa.edu.ar>
2021-04-09 18:54:05 -0300

    link

--- proyectoIntegrador/app.js
+++ proyectoIntegrador/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productRouter = require('./routes/products');
+var profileRouter = require('./routes/profile');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/product', productRouter);
+app.use('/profile', profileRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
