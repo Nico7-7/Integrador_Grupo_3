d5e2b12a1432f89a993af144f72ec4d7785708f4
Nicolas Cavalieri <ncavalieri@udesa.edu.ar>
2021-04-18 00:15:36 -0300

    Datos

---
+++ proyectoIntegrador/data/productos.js
@@ -0,0 +1,70 @@
+let cripto = {
+    lista: [
+        {
+            id: 1,
+            nombre: "Cardano",
+            abreviacion: "ADA",
+            descripcion: "Una plataforma de blockchain de código abierto que busca redistribuir el poder de las estructuras contables entre los individuos, ayudando a crear una sociedad más segura, transparente y justa.",
+            precio: 1.37,
+            comentarios: 26
+        },
+        {
+            id: 2,
+            nombre: "Chainlink",
+            abreviacion: "Link",
+            descripcion: "Una red de oráculo descentralizado que tiene como objetivo conectar contratos inteligentes con datos del mundo real.",
+            precio: 39.39,
+            comentarios: 23
+        },
+        {
+          id: 3,
+          nombre: "Ethreum",  
+          abreviacion: "ETH",
+          descripcion: "Un sistema blockchain descentralizado que funciona como plataforma para intercambiar numerosas criptomonedas, así como para la ejecución de contratos inteligentes.",
+          precio: 2305.97,
+          comentarios: 19
+        },
+        {
+            id: 4,
+            nombre: "Polkadot",  
+            abreviacion: "DOT",
+            descripcion: "Un protocolo de sharding multicadena que facilita la transferencia entre cadenas de cualquier tipo de datos o activos, no solo tokens.",
+            precio: 41.84,
+            comentarios: 18
+        },
+        {
+            id: 5,
+            nombre: "Chain games",  
+            abreviacion: "CHAIN",
+            descripcion: "Un protocolo para apuestas en directo para competiciones de e-sports, a través de contratos inteligentes decentralizados.",
+            precio: 0.3466,
+            comentarios: 8
+        },
+        {
+            id: 6,
+            nombre: "Decentraland",  
+            abreviacion: "Mana",
+            descripcion: "Una plataforma de realidad virtual desarrollada por el blockchain de Ethereum, en donde se puede crear, experimentar y monetizar apps y contenido.",
+            precio: 1.36,
+            comentarios: 10
+        },
+        {
+            id: 7,
+            nombre: "StormX",  
+            abreviacion: "STMX",
+            descripcion: "Una app para smartphones con el objetivo de ofrecerle a sus usuarios una devolución de dinero por sus compras online con cripto.",
+            precio: 0.05995,
+            comentarios: 3
+        },
+        {
+            id: 8,
+            nombre: "Ecomi",  
+            abreviacion: "OMI",
+            descripcion: "Una ventanilla única para coleccionables digitales a través de la aplicación VE-VE que lleva la cultura pop y el entretenimiento al siglo XXI.",
+            precio: 0.0076,
+            comentarios: 1
+        },
+    ]
+}
+
+module.exports = cripto;
