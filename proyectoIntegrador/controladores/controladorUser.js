81b63863f843b6324c84306b806f51bff2a2a658
catarosella <crosella@udesa.edu.ar>
2021-04-23 23:13:28 -0300

    fin

--- proyectoIntegrador/controladores/controladorUser.js
+++ proyectoIntegrador/controladores/controladorUser.js
@@ -1,17 +1,15 @@
 const usuarios = require("../data/usuarios")
 const comentarios = require('../data/comentarios')
+const products = require('../data/productos')
 
 const controller = {
     profile: function(req, res, next){
         let id = req.params.id;
-        for(let i = 0; i < usuarios.length; i++){
-            if(usuarios[i].id == id){
                 res.render('profile', {
                     'usuarios': usuarios.lista,
-                    'idUsuario': id
+                    'idUsuario': id,
+                    'producto_usuario': products.lista
                 })
-            }
-        }
     },
     login: function(req, res, next){
         res.render('login', {
--- proyectoIntegrador/data/productos.js
+++ proyectoIntegrador/data/productos.js
@@ -5,56 +5,64 @@ let cripto = {
             nombre: "Cardano",
             descripcion: "Una plataforma de blockchain de código abierto que busca redistribuir el poder de las estructuras contables entre los individuos, ayudando a crear una sociedad más segura, transparente y justa.",
             imagen: "https://s2.coinmarketcap.com/static/img/coins/200x200/2010.png",
-            comentarios: 14
+            comentarios: 14,
+            id_usuario: 14
         },
         {
             id: 2,
             nombre: "Chainlink",
             descripcion: "Una red de oráculo descentralizado que tiene como objetivo conectar contratos inteligentes con datos del mundo real.",
             imagen: "https://s2.coinmarketcap.com/static/img/coins/200x200/1975.png",
-            comentarios: 23
+            comentarios: 23,
+            id_usuario: 11
         },
         {
             id: 3,
             nombre: "Ethereum",  
             descripcion: "Un sistema blockchain descentralizado que funciona como plataforma para intercambiar numerosas criptomonedas, así como para la ejecución de contratos inteligentes.",
             imagen: "https://64.media.tumblr.com/18a5246a4287ee4db0cd134d69954eb0/tumblr_inline_o4wvui8rKt1r2w261_400.png",
-            comentarios: 19
+            comentarios: 19,
+            id_usuario: 10
         },
         {
             id: 4,
             nombre: "Polkadot",  
             descripcion: "Un protocolo de sharding multicadena que facilita la transferencia entre cadenas de cualquier tipo de datos o activos, no solo tokens.",
             imagen: "https://weusdata.com/wp-content/uploads/2021/01/Polkadot.png",
-            comentarios: 18
+            comentarios: 18,
+            id_usuario: 10
         },
         {
             id: 5,
             nombre: "Chain games",
             descripcion: "Un protocolo para apuestas en directo para competiciones de e-sports, a través de contratos inteligentes decentralizados.",
             imagen: "https://coincost.net/uploads/temp/04178d6b96b8c23fbf596de6000a1a72.png",
-            comentarios: 8
+            comentarios: 8,
+            id_usuario: 2
         },
         {
             id: 6,
             nombre: "Decentraland",  
             descripcion: "Una plataforma de realidad virtual desarrollada por el blockchain de Ethereum, en donde se puede crear, experimentar y monetizar apps y contenido.",
             imagen: "https://assets.coingecko.com/coins/images/878/large/decentraland-mana.png?1550108745",
-            comentarios: 10
+            comentarios: 10,
+            id_usuario: 11
         },
         {
             id: 7,
             nombre: "StormX",  
             descripcion: "Una app para smartphones con el objetivo de ofrecerle a sus usuarios una devolución de dinero por sus compras online con cripto.",
             imagen: "https://assets.coingecko.com/coins/images/1369/large/StormX.png?1603113002",
-            comentarios: 3
+            comentarios: 3,
+            id_usuario: 1
         },
         {
             id: 8,
             nombre: "Ecomi",  
             descripcion: "Una ventanilla única para coleccionables digitales a través de la aplicación VE-VE que lleva la cultura pop y el entretenimiento al siglo XXI.",
             imagen: "https://icoholder.com/files/img/0c374147dd170d41a31f5c0083059500.jpeg",
-            comentarios: 1
+            comentarios: 1,
+            id_usuario: 1
         },
     ]
 }
