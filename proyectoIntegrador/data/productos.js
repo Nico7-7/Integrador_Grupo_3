818fd00c1a8a9c407cc36733d3286373f32df3ec
BautiM37 <bmigliore@udesa.edu.ar>
2021-04-24 21:01:36 -0300

    73 comentarios nuevos y un par de ajustes. Todo en phpmyadmin.

--- proyectoIntegrador/data/productos.js
+++ proyectoIntegrador/data/productos.js
@@ -5,7 +5,7 @@ let cripto = {
             nombre: "Cardano",
             descripcion: "Una plataforma de blockchain de código abierto que busca redistribuir el poder de las estructuras contables entre los individuos, ayudando a crear una sociedad más segura, transparente y justa.",
             imagen: "https://s2.coinmarketcap.com/static/img/coins/200x200/2010.png",
-            comentarios: 14,
+            comentarios: 10,
             id_usuario: 14
         },
         {
@@ -13,7 +13,7 @@ let cripto = {
             nombre: "Chainlink",
             descripcion: "Una red de oráculo descentralizado que tiene como objetivo conectar contratos inteligentes con datos del mundo real.",
             imagen: "https://s2.coinmarketcap.com/static/img/coins/200x200/1975.png",
-            comentarios: 23,
+            comentarios: 9,
             id_usuario: 11
         },
         {
@@ -21,7 +21,7 @@ let cripto = {
             nombre: "Ethereum",  
             descripcion: "Un sistema blockchain descentralizado que funciona como plataforma para intercambiar numerosas criptomonedas, así como para la ejecución de contratos inteligentes.",
             imagen: "https://64.media.tumblr.com/18a5246a4287ee4db0cd134d69954eb0/tumblr_inline_o4wvui8rKt1r2w261_400.png",
-            comentarios: 19,
+            comentarios: 11,
             id_usuario: 10
         },
         {
@@ -29,7 +29,7 @@ let cripto = {
             nombre: "Polkadot",  
             descripcion: "Un protocolo de sharding multicadena que facilita la transferencia entre cadenas de cualquier tipo de datos o activos, no solo tokens.",
             imagen: "https://weusdata.com/wp-content/uploads/2021/01/Polkadot.png",
-            comentarios: 18,
+            comentarios: 7,
             id_usuario: 10
         },
         {
@@ -37,7 +37,7 @@ let cripto = {
             nombre: "Chain games",
             descripcion: "Un protocolo para apuestas en directo para competiciones de e-sports, a través de contratos inteligentes decentralizados.",
             imagen: "https://coincost.net/uploads/temp/04178d6b96b8c23fbf596de6000a1a72.png",
-            comentarios: 8,
+            comentarios: 5,
             id_usuario: 2
         },
         {
@@ -45,7 +45,7 @@ let cripto = {
             nombre: "Decentraland",  
             descripcion: "Una plataforma de realidad virtual desarrollada por el blockchain de Ethereum, en donde se puede crear, experimentar y monetizar apps y contenido.",
             imagen: "https://assets.coingecko.com/coins/images/878/large/decentraland-mana.png?1550108745",
-            comentarios: 10,
+            comentarios: 4,
             id_usuario: 11
         },
         {
@@ -53,7 +53,7 @@ let cripto = {
             nombre: "StormX",  
             descripcion: "Una app para smartphones con el objetivo de ofrecerle a sus usuarios una devolución de dinero por sus compras online con cripto.",
             imagen: "https://assets.coingecko.com/coins/images/1369/large/StormX.png?1603113002",
-            comentarios: 3,
+            comentarios: 4,
             id_usuario: 1
         },
         {
@@ -61,7 +61,7 @@ let cripto = {
             nombre: "Ecomi",  
             descripcion: "Una ventanilla única para coleccionables digitales a través de la aplicación VE-VE que lleva la cultura pop y el entretenimiento al siglo XXI.",
             imagen: "https://icoholder.com/files/img/0c374147dd170d41a31f5c0083059500.jpeg",
-            comentarios: 1,
+            comentarios: 5,
             id_usuario: 1
         },
     ]
