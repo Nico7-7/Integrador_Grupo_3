d6d2830efadf1b24f7b37a06a77600ab5b82bb45
catarosella <crosella@udesa.edu.ar>
2021-06-10 17:05:20 -0300

    arreglo puerto

--- proyectoIntegrador/database/config/config.js
+++ proyectoIntegrador/database/config/config.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 module.exports = {
   "development": {
     "username": "root",
     "password": "root",
     "database": "proyecto_integrador",
     "host": "127.0.0.1",
-    "port": "8889",
+    "port": process.env.DB_PORT,
     "dialect": "mysql",
     "operatorsAliases": false
   }
