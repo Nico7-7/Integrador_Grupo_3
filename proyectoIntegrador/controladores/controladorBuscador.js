5ea92b7f05d62b16541f98bad74fa68547cebeee
Nicolas Cavalieri <ncavalieri@udesa.edu.ar>
2021-06-11 17:36:52 -0300

    Buscador

--- proyectoIntegrador/controladores/controladorBuscador.js
+++ proyectoIntegrador/controladores/controladorBuscador.js
@@ -1,11 +1,19 @@
-const productos = require('../database/models')
+const db = require('../database/models');
+const op = db.Sequelize.Op;
 
 const controller = {
     search: function(req, res, next){
         let buscador = req.query.search;
-        res.render('search-results', {
-            'busqueda': buscador,
-            'producto': productos.lista
+        db.Producto.findAll({
+            where: [
+                {nombre_producto: {[op.like]: '%'+buscador+'%'}}
+            ]
+        })
+        .then((cripto)=> {
+            return res.render('search-results', {
+                cripto: cripto,
+                buscador: buscador
+            })
         })
     }
 }
